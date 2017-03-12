df5b3168dcd46ee89d0e5729db89a104b66b8706
irfan mohammad <irfan.mohammad@trinet.com>
2017-03-12 09:54:54 +0530

    read file from server

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var http = require('http');
-
+var fs = require('fs');         // To fead files from server
 http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello World');
-    console.log('hi');
-    response.end();
-}).listen(3000);;
+    response.writeHead(200, {'Content-Type': 'text/html'}); // help to render type which is mention like html,plain,etc
+
+    //read a file from srever and send it as response
+    fs.readFile('./index.html',null,function(error,data){
+        if(error){
+            response.writeHead(400);
+            response.write('File not Found!');
+            console.log('File not found');
+        }else{
+            response.write(data); // Here data holds index file data
+        }
+        response.end();
+    });
+}).listen(3000);
